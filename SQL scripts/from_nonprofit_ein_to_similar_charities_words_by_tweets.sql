58dff4053174e2b41999d9e4d9d7422a0641ab7b
John Byron <giorgio.cavaggion@gmail.com>
2013-07-26 17:01:53 -0500

    Query from nonprofit to words of similar nonprofits
    by tweets given ein

---
+++ SQL scripts/from_nonprofit_ein_to_similar_charities_words_by_tweets.sql
@@ -0,0 +1,19 @@
+SELECT my_table.nonprofits_id, my_table.charity, my_table.similarity, words.* 
+FROM (
+	SELECT nonpr.nonprofits_id, nonpr.name as charity,  sim.similarity , nonpr.twitter_name as twitter_name
+	FROM giving.nonprofits_similarity_by_tweets as sim, giving.nonprofits as nonpr, giving.nonprofits as nonpr2
+	WHERE nonpr2.ein = 'XX-XXXXXXX' 
+		and sim.twitter_name1 = nonpr2.twitter_name and sim.twitter_name2 = nonpr.twitter_name 
+ 	
+	UNION
+
+	SELECT nonpr.nonprofits_id, nonpr.name as charity, sim.similarity, nonpr.twitter_name as twitter_name
+	FROM giving.nonprofits_similarity_by_tweets as sim, giving.nonprofits as nonpr, giving.nonprofits as nonpr2
+	WHERE nonpr2.ein = 'XX-XXXXXXX' 
+		and sim.twitter_name2 = nonpr2.twitter_name and sim.twitter_name1 = nonpr.twitter_name  
+	
+	ORDER BY similarity DESC
+	LIMIT 10
+) as my_table
+JOIN giving.nonprofits_top_words_tweets as words on words.twitter_name = my_table.twitter_name
+ORDER BY similarity DESC
