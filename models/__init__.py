2a9915c2155fdca9309b154f26098b875965bf73
John Brock <jhbrock@ics.uci.edu>
2013-07-27 18:10:53 -0500

    Adding SQLAlchemy support and logic for updating nonprofits in the DB based on GuideStar API results.

---
+++ models/__init__.py
@@ -0,0 +1,52 @@
+from causenet import config
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker as sessionmakermaker
+
+host = config.read_config('database', 'host')
+db_name = config.read_config('database', 'database')
+user = config.read_config('database', 'user')
+password = config.read_config('database', 'pass')
+connection_string = 'mysql+mysqldb://{0}:{1}@{2}/{3}'.format(user, password, host, db_name)
+engine = create_engine(connection_string, encoding='latin1', convert_unicode=True)
+# yep, it's latin1. Check it:
+# mysql -h host-stuff-here.rds.amazonaws.com -u ourusername -p
+# SHOW DATABASES;
+# USE THEDATABASEWITHSTUFFINIT;
+# SHOW VARIABLES LIKE "character_set_database";
+sessionmaker = sessionmakermaker(bind=engine)
+DBSession = sessionmaker()
+
+Base = declarative_base()
+
+
+class Nonprofit(Base):
+    # __table__ = Table('tweets', metadata, autoload=True)
+    __tablename__ = 'nonprofits'
+    nonprofits_id = Column(Integer, primary_key=True)
+    name = Column(String)
+    ein = Column(String)
+    ntee_code = Column(String)
+    mission_statement = Column(String)
+    description = Column(String)
+    twitter_id = Column(String)
+    twitter_name = Column(String)
+    city = Column(String)
+    state = Column(String)
+    ZIP = Column(String)
+    guidestar_participation_level = Column(String)
+    guidestar_url = Column(String)
+
+    def __init__(self, name, ein, ntee_code, mission_statement, description, twitter_id, twitter_name, city, state, ZIP, guidestar_participation_level, guidestar_url):
+        self.name = name
+        self.ein = ein
+        self.ntee_code = ntee_code
+        self.mission_statement = mission_statement
+        self.description = description
+        self.twitter_id = twitter_id
+        self.twitter_name = twitter_name
+        self.city = city
+        self.state = state
+        self.ZIP = ZIP
+        self.guidestar_participation_level = guidestar_participation_level
+        self.guidestar_url = guidestar_url
--- nonprofits_updater.py
+++ nonprofits_updater.py
@@ -1,4 +1,33 @@
 import guidestar.search
-import sqlalchemy
+from causenet.models import DBSession, Nonprofit
 
-def add_nonprofit_info_to_db(overwrite_existing_info):
+
+def add_nonprofit_info_to_db(ein):
+    query = DBSession.query(Nonprofit).filter(Nonprofit.ein == ein)
+    nonprofit_db = query.first()
+    nonprofit_gs = guidestar.search.get_nonprofit(ein)
+    if nonprofit_db is None:
+        nonprofit_db = Nonprofit(nonprofit_gs.name,
+                                 nonprofit_gs.ein,
+                                 nonprofit_gs.ntee_code,
+                                 nonprofit_gs.mission,
+                                 nonprofit_gs.mission,
+                                 None,
+                                 None,
+                                 nonprofit_gs.city,
+                                 nonprofit_gs.state,
+                                 nonprofit_gs.zip_code,
+                                 nonprofit_gs.guidestar_participation,
+                                 nonprofit_gs.guidestar_url)
+        DBSession.add(nonprofit_db)
+    else:
+        nonprofit_db.name = nonprofit_gs.name
+        nonprofit_db.ntee_code = nonprofit_gs.ntee_code
+        nonprofit_db.mission = nonprofit_gs.mission
+        nonprofit_db.description = nonprofit_gs.mission
+        nonprofit_db.city = nonprofit_gs.mission
+        nonprofit_db.state = nonprofit_gs.mission
+        nonprofit_db.ZIP = nonprofit_gs.zip_code
+        nonprofit_db.guidestar_participation_level = nonprofit_gs.guidestar_participation
+        nonprofit_db.guidestar_url = nonprofit_gs.guidestar_url
+    DBSession.commit()
