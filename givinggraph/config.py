d9a67b3aea02841b6929150ad80fd92b5f4b8b8b
Aron Culotta <aronwc@gmail.com>
2013-07-30 14:15:14 -0500

    remove argparse bit from config

--- givinggraph/config.py
+++ givinggraph/config.py
@@ -1,21 +1,11 @@
 from requests_oauthlib import OAuth1
-import argparse
 import ConfigParser
 import os
-
-ap = argparse.ArgumentParser(description=__doc__,
-                             formatter_class=argparse.RawTextHelpFormatter)
-ap.add_argument('--config',
-                metavar='CONFIG',
-                default=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.cfg'),
-                help='config file, containing keys and login info')
-args = ap.parse_args()
 
 
 def read_config(section_name, item_name):
     config = ConfigParser.RawConfigParser()
     config.read(os.environ['GGRAPH_CFG'])
-    #config.read(args.config)
     return config.get(section_name, item_name)
 
 
