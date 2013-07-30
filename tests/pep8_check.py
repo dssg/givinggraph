6d862fc9c9b6547f1cceeaaedbefef0e4fbfaec8
Aron Culotta <aronwc@gmail.com>
2013-07-30 15:30:28 -0500

    pep8 test tweaks

--- tests/pep8_check.py
+++ tests/pep8_check.py
@@ -2,14 +2,12 @@ import os
 import unittest
 import pep8
 
+import givinggraph
+
 
 class TestFormatting(unittest.TestCase):
     def test_pep8(self):
-        basepath = os.getcwd()
-        print 'Running PEP-8 checks on path', basepath
-        pep8style = pep8.StyleGuide(paths=[basepath], ignore=['E128', 'E501'])
-        report = pep8style.check_files()
-        if report.total_errors:
-            print report.total_errors
-
-        self.assertEqual(report.total_errors, 0, 'Codebase does not pass PEP-8')
+        print 'Running PEP-8 checks on path', givinggraph.root
+        pep8style = pep8.StyleGuide(paths=[givinggraph.root], ignore=['E128', 'E501'])
+        total_errors = pep8style.check_files().total_errors
+        self.assertEqual(total_errors, 0, 'Codebase does not pass PEP-8 (%d errors)' % total_errors)
