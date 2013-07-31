48012a1a6b68d4a61cfb70b425e6334bf0675441
John Brock <jhbrock@ics.uci.edu>
2013-07-31 14:46:31 -0500

    Replacing incorrect parsing of Yahoo search results and moving common logic for searching Yahoo into a single helper function. Fixes #1.

--- givinggraph/yahoo/search.py
+++ givinggraph/yahoo/search.py
@@ -1,17 +1,15 @@
-import re
 import requests
 from BeautifulSoup import BeautifulSoup as bs
 from urllib import urlencode
 
 
 def get_search_results(search_terms):
-    URL = 'http://search.yahoo.com/search?'
-    fields = {'p': search_terms.strip()}
-    soup = bs(requests.get(URL + urlencode(fields)).text)
-    return [re.sub('<[^>]+>', '', str(hit.find(attrs={'class': 'url'}))) for hit in soup.findAll(attrs={'class': 'res'})]
+    return __search__('http://search.yahoo.com/search?', search_terms)
 
 def get_news_results(search_terms):
-    URL = 'http://news.search.yahoo.com/search?'
-    fields = {'p': search_terms.strip()}
-    soup = bs(requests.get(URL + urlencode(fields)).text)
+    return __search__('http://news.search.yahoo.com/search?', search_terms)
+
+def __search__(base_url, search_terms):
+    fields = {'p': search_terms.strip(), 'n': 30}  # n is for the number of search results to display on one page
+    soup = bs(requests.get(base_url + urlencode(fields)).text)
     return [hit['href'] for hit in soup.findAll(attrs={'class': 'yschttl spt'}) if hit['href'][:7] == 'http://']
