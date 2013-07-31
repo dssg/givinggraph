cf36386bf387bac2237ef89fd4c614def9cf0001
John Brock <jhbrock@ics.uci.edu>
2013-07-31 10:42:53 -0500

    Replacing for loop in twitter_get(...) with call to urllib.urlencode. Should help with escaping weird characters.

--- givinggraph/twitter/common.py
+++ givinggraph/twitter/common.py
@@ -1,16 +1,12 @@
 import givinggraph.config
 import requests
-
+from urllib import urlencode
 
 # url should be the base URL ending in .json
 # params is a dictionary where the keys and values are strings
 # returns json if the response is OK, returns None is the response is not OK
 def twitter_get(url, params):
-    complete_url = url + '?'
-    for key in params:
-        complete_url += key + '=' + str(params[key]) + '&'
-    complete_url = complete_url.strip('&')
-
+    complete_url = url + '?' + urlencode(params)
     r = requests.get(complete_url, auth=givinggraph.config.get_twitter_authentication())
     if r.status_code == 200:
         return r
