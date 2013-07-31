25251bffb3d42fa964d81cce1f6d56edbcadcdc0
John Brock <jhbrock@ics.uci.edu>
2013-07-31 10:31:53 -0500

    Adding get_news_results(...) function to yahoo.search module.

--- givinggraph/yahoo/search.py
+++ givinggraph/yahoo/search.py
@@ -9,3 +9,9 @@ def get_search_results(search_terms):
     fields = {'p': search_terms.strip()}
     soup = bs(requests.get(URL + urlencode(fields)).text)
     return [re.sub('<[^>]+>', '', str(hit.find(attrs={'class': 'url'}))) for hit in soup.findAll(attrs={'class': 'res'})]
+
+def get_news_results(search_terms):
+    URL = 'http://news.search.yahoo.com/search?'
+    fields = {'p': search_terms.strip()}
+    soup = bs(requests.get(URL + urlencode(fields)).text)
+    return [hit['href'] for hit in soup.findAll(attrs={'class': 'yschttl spt'}) if hit['href'][:7] == 'http://']
