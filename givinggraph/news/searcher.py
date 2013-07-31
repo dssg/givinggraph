e25629fd88c8d303f77ec2e45e42296fcb9fbd82
John Brock <jhbrock@ics.uci.edu>
2013-07-31 15:24:20 -0500

    Filling out logic for news.searcher.find_news_articles(...).

--- givinggraph/news/searcher.py
+++ givinggraph/news/searcher.py
@@ -1,2 +1,16 @@
-def find_news_article_urls(ein):
-    pass  # <-----insert magic here
+import givinggraph.yahoo.search as yahoo
+import givinggraph.news.parser as parser
+import requests
+from collections import namedtuple
+
+Article = namedtuple('Article', 'url headline body')
+
+
+def find_news_articles(nonprofit_name, urls_to_ignore=[]):
+    articles = []
+    for url in yahoo.get_news_results(nonprofit_name):
+        if url not in urls_to_ignore:
+            html = requests.get(url).text
+            headline, body = parser.get_article_parts(html)
+            articles.append(Article(url, headline, body))
+    return articles
