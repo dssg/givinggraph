75eb8dfd627ed837dcbeef03c6d3ab76f2cfb9c7
John Brock <jhbrock@ics.uci.edu>
2013-08-02 12:27:04 -0500

    PEP-8 test no longer ignores E128 (continuation line under-indented for visual indent). I think this is actually a good style choice, it makes it easier to quickly read arguments on multiple lines.

--- tests/pep8_check.py
+++ tests/pep8_check.py
@@ -8,6 +8,6 @@ import givinggraph
 class TestFormatting(unittest.TestCase):
     def test_pep8(self):
         print 'Running PEP-8 checks on path', givinggraph.root
-        pep8style = pep8.StyleGuide(paths=[givinggraph.root], ignore=['E128', 'E501'])
+        pep8style = pep8.StyleGuide(paths=[givinggraph.root], ignore=['E501'])  # E501 doesn't allow long lines.
         total_errors = pep8style.check_files().total_errors
         self.assertEqual(total_errors, 0, 'Codebase does not pass PEP-8 (%d errors)' % total_errors)
