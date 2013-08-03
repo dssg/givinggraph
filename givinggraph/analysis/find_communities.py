4488d8b7d4793b7bc329ada6517af4337d2bdae2
John Byron <giorgio.cavaggion@gmail.com>
2013-08-02 23:18:56 -0500

    Used function which automatically load edges

--- givinggraph/analysis/find_communities.py
+++ givinggraph/analysis/find_communities.py
@@ -7,8 +7,7 @@ edges_list = [("123", "345", 0.5), ("345", "456", 0.7),
 
 def compute_communities(edges_list):
     G = nx.Graph()
-    for edge in edges_list:
-        G.add_edge(edge[0], edge[1], weight=edge[2])
+    G.add_weighted_edges_from(edges_list)
     partition = community.best_partition(G)
     return partition
 
