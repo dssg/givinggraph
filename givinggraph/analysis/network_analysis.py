87c54d442b384a61e75ed89b117c214a94750d39
John Byron <giorgio.cavaggion@gmail.com>
2013-08-06 14:28:47 -0500

    Added export function to network analysis module

--- givinggraph/analysis/network_analysis.py
+++ givinggraph/analysis/network_analysis.py
@@ -5,31 +5,37 @@ edges_list = [("123", "345", 0.5), ("345", "456", 0.7),
               ("345", "1", 0.1), ("1", "123", 0.3)]
 
 
-def compute_communities(edges_list, directed=False, weighted=True):
-    """For the given weighted edges, find the communities for each node."""
+# first create a Graph, then give the graph as a parameter to the other functions
+def create_graph(edges_list, directed=False, weighted=True):
+    """For the given weighted edges, create a graph object"""
     G = nx.DiGraph() if directed else nx.Graph()
     if weighted:
         G.add_weighted_edges_from(edges_list)
     else:
         G.add_edges_from(edges_list)
+    return G
 
+
+def compute_communities(G):
+    """For the given graph, find the communities for each node."""
+    if isinstance(G, nx.DiGraph):
+        G = G.to_undirected()
     partition = community.best_partition(G)
     return partition
 
 
-def compute_hubs_authorities(edges_list, directed=False, weighted=True):
-    """For the given weighted edges, compute hubs and authorities values for each node"""
-    G = nx.DiGraph() if directed else nx.Graph()
-    if weighted:
-        G.add_weighted_edges_from(edges_list)
-    else:
-        G.add_edges_from(edges_list)
-
+def compute_hubs_authorities(G):
+    """For the given graph, compute hubs and authorities values for each node"""
     hits = nx.hits(G, max_iter=500)
     hubs = hits[0]
     authorities = hits[1]
     return hubs, authorities
 
+
+def export_graph(G, name='graph'):
+    nx.write_gml(G, name + '.gml')
+
 # print compute_hubs_authorities(edges_list)[0]
 # print compute_hubs_authorities(edges_list)[1]
-print compute_communities(edges_list)
+my_graph = create_graph(edges_list)
+print compute_communities(my_graph)
