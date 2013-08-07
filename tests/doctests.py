16fc53d762eb2d761871c28129f971b3ba896fc1
Aron Culotta <aronwc@gmail.com>
2013-08-06 23:25:03 -0500

    remove doctests.py, since nose will find all automatically

--- tests/doctests.py
+++
@@ -1,9 +0,0 @@
-import doctest
-import givinggraph.news.parser
-import givinggraph.util.text2tfidf
-
-
-def load_tests(loader, tests, ignore):
-    tests.addTests(doctest.DocTestSuite(givinggraph.news.parser))
-    tests.addTests(doctest.DocTestSuite(givinggraph.util.text2tfidf))
-    return tests
