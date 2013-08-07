0ef18a1ee5ab5b1c08dbf261269546e99e57c30a
John Brock <jhbrock@uci.edu>
2013-08-07 13:29:20 -0500

    Adding since_id parameter to get_tweets(...) so that already-retrieved tweets will not be retrieved.

--- givinggraph/twitter/tweets.py
+++ givinggraph/twitter/tweets.py
@@ -1,12 +1,13 @@
 from givinggraph.twitter.common import twitter_get
 
 
-def get_tweets(user_id, include_retweets):
+def get_tweets(user_id, include_retweets, since_id=0):
     """Takes a Twitter user id (a string) as input and returns all of that user's tweets, going back as far as the API will let you."""
     base_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
     params = {'user_id': user_id,
               'include_rts': include_retweets,
-              'count': 200}
+              'count': 200,
+              'since_id': since_id}
     all_tweets = []
     while True:
         chunk_of_tweets = twitter_get(base_url, params, 5)
