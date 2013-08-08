fbeaf0db23d072644c204c03836d1c179d62bdbc
John Brock <jhbrock@uci.edu>
2013-08-08 16:05:07 -0500

    Now you can retrieve tweets by either the Twitter user ID or the Twitter screen name.

--- givinggraph/twitter/tweets.py
+++ givinggraph/twitter/tweets.py
@@ -1,13 +1,26 @@
 from givinggraph.twitter.common import twitter_get
 
 
-def get_tweets(user_id, include_retweets, since_id=0):
-    """Takes a Twitter user id (a string) as input and returns all of that user's tweets, going back as far as the API will let you."""
-    base_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+def get_tweets_by_id(user_id, include_retweets, since_id=0):
+    """Takes a Twitter user id (a string) as input and returns all of that user's tweets. If since_id is not set, it will go back as far as the API will let you."""
     params = {'user_id': user_id,
               'include_rts': include_retweets,
-              'count': 200,
               'since_id': since_id}
+    return __get_tweets__(params)
+
+
+def get_tweets_by_name(screen_name, include_retweets, since_id=0):
+    """Takes a Twitter screen name as input and returns all of that user's tweets. If since_id is not set, it will go back as far as the API will let you."""
+    params = {'screen_name': screen_name,
+              'include_rts': include_retweets,
+              'since_id': since_id}
+    return __get_tweets__(params)
+
+
+def __get_tweets__(params):
+    """Takes a Twitter user id (a string) or screen name as input (screen_name takes precedent) and returns all of that user's tweets, going back as far as the API will let you."""
+    base_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+    params['count'] = 200
     all_tweets = []
     while True:
         chunk_of_tweets = twitter_get(base_url, params, 5)
