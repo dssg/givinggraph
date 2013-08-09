720f3833dbb1bf4a2740146f6e3642815d4e5cc2
Aron Culotta <aronwc@gmail.com>
2013-08-09 13:08:28 -0500

    app.py now uses sqlalchemy

--- givinggraph/api/app.py
+++ givinggraph/api/app.py
@@ -2,52 +2,35 @@
 Set of RESTful endpoints to query the giving database.  See README.md for
 documentation.
 '''
-# FIXME: Use SQLAlchemy instead of raw sql
+# FIXME: Fail gracefully on no results.
 
 
 import json
 
 from flask import Flask
-import MySQLdb
-import MySQLdb.cursors
 
-import givinggraph.config as config
+from givinggraph.models import DBSession, Nonprofit, Company, News_Article, Nonprofits_Similarity_By_Description, Nonprofits_Similarity_By_Tweets
+
 
 app = Flask(__name__)
-db = None
 
 
-def select(stmt, args):
-    'Return results of a select query.'
-    cur = db.cursor()
-    cur.execute(stmt, args)
-    return cur.fetchall()
+def result2json(result):
+    print result.__dict__
+    return json.dumps(dict([(k, v) for k, v in result.__dict__.iteritems() if not k.startswith('_')]))
 
 
 @app.route('/nonprofit/ein/<ein_id>')
 def by_ein(ein_id):
-    '''lookup nonprofit by EIN.'''
-    return json.dumps(select('select * from nonprofits where ein=%s', (ein_id)))
+    """Lookup nonprofit by EIN."""
+    return result2json(DBSession.query(Nonprofit).filter(Nonprofit.ein == ein_id).first())
 
 
 @app.route('/nonprofit/id/<nonprofit_id>')
 def by_id(nonprofit_id):
-    '''lookup nonprofit by our internal id. Note that this violates DRY a bit
-    (wrt to by_ein) to avoid SQL injection on column names.'''
-    return json.dumps(select('select * from nonprofits where nonprofits_id=%s',
-                             (nonprofit_id)))
-
-
-def init_db():
-    'Create connection to the mysql database specified in --config.'
-    global db
-    db = MySQLdb.connect(host=config.read_config('database', 'host'),
-                         user=config.read_config('database', 'user'),
-                         passwd=config.read_config('database', 'pass'),
-                         db=config.read_config('database', 'database'),
-                         cursorclass=MySQLdb.cursors.DictCursor)
+    """Lookup nonprofit by our internal id."""
+    return result2json(DBSession.query(Nonprofit).filter(Nonprofit.nonprofits_id == nonprofit_id).first())
 
 
 if __name__ == '__main__':
-    init_db()
     app.run(debug=True)
