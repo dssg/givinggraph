34a7dae4de061701bf961878962013d091590aa2
Aron Culotta <aronwc@gmail.com>
2013-08-08 22:50:25 -0500

    predict cause of company from text

--- givinggraph/analysis/company_cause_classifier.py
+++ givinggraph/analysis/company_cause_classifier.py
@@ -2,6 +2,8 @@
 train a model based on the data at milliondollarlist.org, which posts
 donations of $1M or more from companies to causes."""
 
+# FIXME: remove company names from web text to make this generalize.
+
 import argparse
 from collections import defaultdict
 import io
@@ -9,12 +11,12 @@ import io
 import numpy as np
 from sklearn import cross_validation, metrics
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.linear_model import LogisticRegression
+#from sklearn.linear_model import LogisticRegression
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.pipeline import Pipeline
+#from sklearn.naive_bayes import MultinomialNB
+#from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelBinarizer, LabelEncoder
-
+from sklearn.svm import LinearSVC
 
 ap = argparse.ArgumentParser(description=__doc__,
                              formatter_class=argparse.RawTextHelpFormatter)
@@ -31,14 +33,17 @@ args = ap.parse_args()
 
 
 def read_causes(filename):
+    causes = set()
     co2causes = defaultdict(lambda: set())
     for line in io.open(filename, mode='rt'):
         parts = line.strip().split('\t')
         co2causes[parts[0]].add(parts[1])
-    return co2causes
+        causes.add(parts[1])
+    return co2causes, causes
 
 
 def read_pages(filename, co2causes):
+    """Read company web page file, retaining only those in co2causes"""
     co2page = dict()
     for line in io.open(filename, mode='rt', encoding='latin_1'):
         parts = line.strip().split('\t')
@@ -47,34 +52,46 @@ def read_pages(filename, co2causes):
     return co2page
 
 
+def print_top_words(vectorizer, clf, class_labels, n=10):
+    """Prints features with the highest coefficient values, per class"""
+    feature_names = vectorizer.get_feature_names()
+    for i, class_label in enumerate(class_labels):
+        topn = np.argsort(clf.coef_[i])[-n:]
+        print("%s: %s" % (class_label,
+              " ".join(feature_names[j] for j in topn)))
+
+
 if (__name__ == '__main__'):
-    company2causes = read_causes(args.causes)
+    company2causes, causes = read_causes(args.causes)
     print 'read %d companies with causes' % len(company2causes.keys())
     company2page = read_pages(args.homepages, company2causes)
     print 'read %d homepages' % len(company2causes.keys())
     companies = company2page.keys()
-    pipeline = Pipeline([('vect', CountVectorizer()),
-                         ('tfidf', TfidfTransformer())])
+    pipeline = CountVectorizer()
     X = pipeline.fit_transform([company2page[c] for c in companies])
-    print X[0]
 
     # convert labels to multilabel format
     Y = np.array([list(company2causes[c]) for c in companies])
     N = len(Y)
-    print Y[0]
     label_enc = LabelEncoder()
-    # FIXME: why are there 83 labels? should be 21
-    Y = label_enc.fit_transform(Y.reshape(-1)).reshape((N, -1))
-    print Y[0]
+    label_enc.fit(list(causes))
+    print 'found %d causes' % len(label_enc.classes_)
+    Y = [list(label_enc.transform(yi)) for yi in Y]
+    print 'labels:', label_enc.classes_
+    # LabelBinarizer buggy with np arrays. See https://github.com/scikit-learn/scikit-learn/issues/856
     Y = LabelBinarizer().fit_transform(Y)
-    print Y[0]
-    clf = OneVsRestClassifier(LogisticRegression())
+    #clf = OneVsRestClassifier(MultinomialNB())
+    #clf = OneVsRestClassifier(LogisticRegression())
+    clf = OneVsRestClassifier(LinearSVC(random_state=0))
 
-    #print cross_validation.cross_val_score(clf, X, Y, cv=10, scoring=metrics.hamming_loss)
     cv = cross_validation.KFold(len(Y), 10, shuffle=True, random_state=1234)
     losses = []
     for train, test in cv:
         truth = Y[test]
         pred = clf.fit(X[train], Y[train]).predict(X[test])
-        losses.append(metrics.hamming_loss(truth, pred))
-    print 'avg hamming los=%.2f stderr=%.2f' % (np.average(losses), np.std(losses))
+        losses.append(metrics.precision_score(truth.reshape(-1), pred.reshape(-1)))
+    print 'F1 score=%.3f stderr=%.3f' % (np.average(losses), np.std(losses))
+
+    # Retrain on all to print top words
+    clf.fit(X, Y)
+    print_top_words(pipeline, clf, label_enc.classes_, 20)
